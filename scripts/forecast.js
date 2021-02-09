201ef23597de3c643b49ec26a656f7ab3def257d
volodymyrianin <volodymyrianin1@gmail.com>
2021-02-09 15:13:09 +0200

    https added

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,7 +4,7 @@ const key = 'zfz4SsIoASuLrncRApJNLtm2GKkpuvWj';
 
 const getWeather = async(cityKey) => {
 
-	const base = `http://dataservice.accuweather.com/currentconditions/v1/`
+	const base = `https://dataservice.accuweather.com/currentconditions/v1/`
 	const query = `${cityKey}?apikey=${key}`;
 
 	const response = await fetch(base + query);
@@ -17,7 +17,7 @@ const getWeather = async(cityKey) => {
 // get city information
 const getCity = async (city) => {
 
-	const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+	const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
 	const query = `?apikey=${key}&q=${city}`;
 
 	const response = await fetch(base + query);
